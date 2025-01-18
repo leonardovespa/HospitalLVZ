0a0eb96d2dd66ed3866d713d717811dafd27ada2
leonardovespa <leonardovespa92@gmail.com>
2025-01-18 12:40:21 +0100

    Se agrega el campo escialidad del médico de la cita solicitada

--- output/Cita.h
+++ output/Cita.h
@@ -9,19 +9,29 @@ class Cita {
     int pacienteID;
     int medicoID;
     string fecha;
+    string especialidad;
     string urgencia;
     string estado;
 
     public:
-    Cita(int citaID, int pacienteID, int medicoID, string fecha, string urgencia);
+    Cita(int citaID, int pacienteID, int medicoID, const string& fecha, const string& especialidad, const string& urgencia);
+    
+    // Obtener información de la cita
+
+    int getCitaID() const;
+    int getPacienteID() const;
+    int getMedicoID() const;
+    string getFecha() const;
+    string getEspecialidad() const;
+    string getUrgencia() const;
+    string getEstado() const;
+
+    //Métodos para gestionar la cita
+
     void modificarCita(string nuevaFecha, string nuevaUrgencia);
     void cancelarCita();
     void mostrarCita() const;
     bool esActiva() const;
-    int getCitaID() const;
-    int getPacienteID() const;
-    int getMedicoID() const;
-    string getEstado() const;
 };
 
 #endif
