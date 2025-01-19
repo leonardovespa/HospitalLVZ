d31eb6332d4c113d1373c267955bbc5f4e522444
leonardovespa <leonardovespa92@gmail.com>
2025-01-19 14:42:22 +0100

    se agregan métodos especificos en paciente.h

--- output/Paciente.h
+++ output/Paciente.h
@@ -18,6 +18,11 @@ public:
     void mostrarDatos() const;
     bool esCronico() const; // Método para verificar si es crónico
 
+    //Metodos para modificar datos especificos
+    void modificarNombre(const string& nuevoNombre);
+    void modificarFechaIngreso(const string& nuevaFechaIngreso);
+    void modificarCronico(bool nuevoEstado);
+
     // Getters
     int getID() const;
     string getNombre() const;
